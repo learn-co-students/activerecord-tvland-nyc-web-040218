ceab6e0f9b264c7329303599015c8ccffa4ae20b
github username <github email address>
2018-04-12 17:33:05 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+  has_many :network, through: :shows
+
+  def say_that_thing_you_say
+    # binding.pry
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
